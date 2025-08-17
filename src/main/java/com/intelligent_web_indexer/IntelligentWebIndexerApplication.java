5c1dcb168bf82726118f2ef840e1b829858ab289
celly <ckavataki@gmail.com>
2025-08-17 11:57:18 +0300

    feat:Created neccessary packages for the project

---
+++ src/main/java/com/intelligent_web_indexer/IntelligentWebIndexerApplication.java
@@ -0,0 +1,13 @@
+package com.intelligent_web_indexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentWebIndexerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentWebIndexerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intelligent_web_indexer/IntelligentWebIndexerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelligent_web_indexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentWebIndexerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
